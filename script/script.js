ed03c566f4804d0250c7da8912a19a91d7a704cd
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-05-23 18:10:52 +0300

    Made recommended edits

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 function showHide(element_id) {
-  var div = document.getElementsByClassName('div');
-  Array.from(div).forEach(element => (element.style.display = 'none'));
+  var div = document.getElementsByClassName("activity-text");
+  Array.from(div).forEach((element) => (element.style.display = "none"));
   if (document.getElementById(element_id)) {
     var obj = document.getElementById(element_id);
-    obj.style.display = 'block';
+    obj.style.display = "block";
   }
 }
